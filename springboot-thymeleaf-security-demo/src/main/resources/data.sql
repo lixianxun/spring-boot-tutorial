2b2a9fe17676f3a0fd7d0a3b87e328bb55ca247a
Administrator <Administrator@SC-201902112059>
2019-03-30 00:29:47 +0800

    add test messages

--- springboot-thymeleaf-security-demo/src/main/resources/data.sql
+++ springboot-thymeleaf-security-demo/src/main/resources/data.sql
@@ -6,10 +6,10 @@ create table if not exists persistent_logins (
   last_used timestamp not null
 );
 
+delete from  role_permission;
 delete from  user_role;
 delete from  roles;
 delete from  users;
-delete from  role_permission;
 delete from  permissions;
 
 INSERT INTO roles (id, name) VALUES 
@@ -39,8 +39,12 @@ insert into role_permission(role_id, permission_id) values
 (1, 1),(1, 2),
 (3, 2);
 
+insert into messages(id, content) values 
+(1, 'hey guru, you are right here!!!'),
+(2, 'hey michael, how are youe!!!')
+;
 
-SELECT u.id, u.name, r.name, p.code, p.url FROM test.users u 
+SELECT u.id, u.name, u.email, r.name, p.code, p.url FROM test.users u 
 left join test.user_role ur on u.id = ur.user_id
 left join test.roles r on ur.role_id=r.id
 left join test.role_permission rp on r.id=rp.role_id
